d650a13eb6f9378edaede3cb01088a131f7731d4
dibbes96 <167886765+dibbes96@users.noreply.github.com>
2026-04-23 18:19:06 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+}, { threshold: 0.2 });
+
+document.querySelectorAll(".feature, #missione, #chi-siamo")
+  .forEach(el => observer.observe(el));
